c7eee72c2a901d515eaffd716faeadea91b45c4e
fieldOfView <aldo@fieldofview.com>
2019-06-13 13:00:06 +0200

    Add the job-name to the title-bar

--- VersionInTitlebarPlugin.py
+++ VersionInTitlebarPlugin.py
@@ -2,22 +2,40 @@
 # The VersionInTitlebarPlugin is released under the terms of the AGPLv3 or higher.
 
 from UM.Extension import Extension
-from UM.Application import Application
+from cura.CuraApplication import CuraApplication
 
 class VersionInTitlebarPlugin(Extension):
     def __init__(self):
         super().__init__()
 
-        self._application = Application.getInstance()
+        self._application = CuraApplication.getInstance()
+
+        self._print_information = None
+        self._main_window = None
+
         self._version_string = self._application.getVersion()
         self._application.engineCreatedSignal.connect(self._engineCreated)
 
+        self._application_name = ""
+
     def _engineCreated(self):
-        # set window title
         try:
-        	engine = Application.getInstance()._qml_engine
+            engine = self._application._qml_engine
         except AttributeError:
-        	engine = Application.getInstance()._engine
-        mainWindow = engine.rootObjects()[0]
-        mainWindow.setTitle(mainWindow.title() + " " + self._version_string)
+            engine = self._application._engine
+        self._main_window = engine.rootObjects()[0]
+
+        try:
+            self._application_name = self._application.getApplicationDisplayName()
+        except AttributeError:
+            self._application_name = self._main_window.title()
+
+        self._print_information = self._application.getPrintInformation()
+        self._print_information.jobNameChanged.connect(self._updateWindowTitle)
+
+        self._updateWindowTitle()
+
+    def _updateWindowTitle(self):
+        # set window title
+        self._main_window.setTitle("%s - %s %s" % (self._print_information.jobName, self._application_name, self._version_string))
 
